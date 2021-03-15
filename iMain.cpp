dee1e6e4e11480f802c741886dc6d4e519fff96a
Muhaiminul-Kabir <muhaiminulkabir32@gmail.com>
2021-03-15 22:25:00 +0600

    GameOver Score Bug Solved

--- iMain.cpp
+++ iMain.cpp
@@ -8,6 +8,7 @@
 #include <list>
 #include<windows.h>
 #include <time.h>
+
 using namespace std;
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::Idraw Here::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::://
 
@@ -22,7 +23,7 @@ bool gameOver = false;
 bool inDis = true;
 bool takeInput = true; //logic for input user name
 bool takeScore = true;
-bool showPassiveMousePosition = true;
+bool showPassiveMousePosition = false;
 //for high score
 long int  point = 0;
 int flag = 0;
@@ -45,11 +46,7 @@ int ix = 0, iy = 0;
 
 int mpx, mpy, count = 0;
 
-string currentPage = "newGame";
-
-char* menuTitles2[6] = { "help", "credits", "highScores", "settings", "newGame", "gameOverPage"};
-char* menuTitles[5] = { "images//help.bmp", "images//menu4.bmp", "images//menu3.bmp", "images//menu2.bmp", "images//menu1.bmp" };
-int totalMenuItems = 5;
+string currentPage = "homePage";
 
 char person_run[2][20] = { "images//b14.bmp", "images//b17.bmp" };
 int runningIndex = 0;
@@ -91,8 +88,12 @@ char explosion[22][25] =
 	"images//explosion11.png",
 
 };
+
+
+int life = 3;
 int roadIndex = 3;
 bool musicOn = true;
+bool gameOverSound = false;
 //int vx = 442, vy = 0;
 int charecterX = (windowWidth / 2) - 80;
 int charecterY = 10;
@@ -116,18 +117,14 @@ bool optionMusicOn = true;
 bool optionDifficulityHigh = false;
 bool optionDifficulityLow = true;
 bool optionDifficulityMedium = false;
-bool gameOverSound = false;
-bool collideMusic = false;
-
 int musicStateIndex = 0;
 int difficulityStateIndex = 0;
 
 
 
-
 void setHigh(char*, long int);
 void showhigh();
-void show(long int, int, int);
+void show(long int ,int ,int );
 int getPercentage(int, int);
 void background();
 void homepage();
@@ -144,81 +141,8 @@ void loadImages();
 void virusFactory();
 
 
-struct MenuItem{
-	char *title;
-	char *backgroundImg;
-	char *hover; //to store hover effect image 
-	int x, y;
-	int width = 400;
-	int height = 50;
-	bool onHoverState = false, firstEntry = true;
+#include "Menu.h";
 
-	MenuItem(){}
-	MenuItem(int x, int y, char *title) : x(x), y(y), title(title), backgroundImg(backgroundImg){
-
-	}
-
-	bool isInsideThis(int mx, int my){ //true if cursor is inside of the MenuItem
-		return (mx >= x && mx <= (x + width)) && (my >= y && my <= (y + height));
-	}
-	void display(){
-		iShowBMP2(x + getPercentage(width, 0), y + getPercentage(height, 40), backgroundImg, 0);
-		if (this->isInsideThis(mpx, mpy))
-			onHoverState = true;
-		else
-			onHoverState = false;
-
-		if (onHoverState){
-			iSetColor(0, 0, 0);
-			//	iFilledRectangle(x, y, width, height);
-			iShowBMP2(x + getPercentage(width, -7), y + getPercentage(height, 40), "images//arr.bmp", 0);
-			iShowBMP2(x + getPercentage(width, 0), y + getPercentage(height, 40), hover, 0);
-			iSetColor(255, 255, 255);
-			if (firstEntry){
-				PlaySound("Sounds\\btn_hover.wav", NULL, SND_ASYNC);
-				firstEntry = false;
-			}
-		}
-		else{
-			//iRectangle(x, y, width, height);
-			firstEntry = true;
-		}
-
-		//iText(x + getPercentage(width, 40), y + getPercentage(height, 40), title, GLUT_BITMAP_HELVETICA_18);
-
-	}
-
-	void cliked(){
-		currentPage = this->title;
-	}
-};
-
-struct Menu{
-	int x = 0, y = 0;
-	int dy = 70, totalItems;
-	int width, height;
-	MenuItem *items;
-
-	Menu(int x, int y, int width, int height, MenuItem items[], int totalItems) :x(x), y(y), width(width), height(height), totalItems(totalItems){
-		this->items = items;
-	}
-
-	void display(){
-		for (int i = 0; i < totalItems; i++){
-			items[i].x = this->x;
-			items[i].y = this->y;
-			items[i].width = this->width;
-			items[i].height = this->height;
-			items[i].title = menuTitles2[i];
-			items[i].hover = hoverImg[i];//hover state image (Muhaiminul kabir)
-			items[i].backgroundImg = menuTitles[i];
-			items[i].display();
-
-			y += dy;
-		}
-		y -= dy*totalItems; // Resetting y
-	}
-};
 struct Track{
 	double x1, x2, y1, y2;
 	double x, y, m;
@@ -264,44 +188,9 @@ struct playerData{
 	char pl[1000];
 	long int scr;
 };
-struct Virus{
-	int image;
-	Track track;
-	int size;
-	double speed;
-	bool hide;
-
-	Virus(){
-		hide = false;
-		speed = 1;
-		size = 50;
-	}
-	void spawn(){
-		if (!hide){
-			setVirusSize();
-			iShowImage(track.getX(), track.getY(), size, size, image);
-			track.speed(speed);
-		}
-		if (track.getY() > Y2) size = 50;
-		else if (track.getY() < Y2 && track.getY() > Y1) size = 75;
-		else if (track.getY() < Y1) size = 100;
-
-		if (track.trackEnded) hide = true;
-
-	}
-
-	void setVirusSize(){
-		if (size == 50) image = virusImg;
-		else if (size == 75) image = virusImg75;
-		else if (size == 100) image = virusImg100;
-		else{
-			cout << "Invalid Virus Size" << endl;
-			exit(1);
-		}
-	}
-};
 
 
+#include "Virus.h"
 
 /***************************Function For SHowing Explosion********************/
 void showExplosion()
@@ -315,7 +204,6 @@ void showExplosion()
 
 		}
 		iShowImage(collisionX, collisionY, 200, 200, id);
-		
 	}
 
 
@@ -585,19 +473,11 @@ void show(long int a, int x, int y)
 	}
 
 }
+
 int getPercentage(int num, int percent){
 	return (int)((num*percent) / 100);
 }
-void background()
-{
-	iFilledRectangle(0, 0, windowWidth, windowHeight);
 
-}
-void homePage(){
-	iShowBMP(0, 0, "images//bg.bmp");
-	menu.display();
-
-}
 
 void lifeIndicator(int life){
 	int dx = 50, i, j;
@@ -623,34 +503,9 @@ void moveRoad(){
 	if (roadIndex <= 0) roadIndex = 3;
 }
 
-void gameOverPage(){
-	if (optionMusicOn == true && gameOver == true && gameOverSound == true){
-		PlaySound("SOUNDS\\gameover.WAV", NULL, 1);
-		gameOverSound = false;
 
-	}
 
-	if (optionMusicOn){
-		musicOn = true;
-	}
-	index0 = 0;
-	userName[index0] = '\0';
-	point = 0;
-	takeInput = true;
-	char p[100];
-	iShowBMP(0, 0, gameOverImg[gameOverIndex]);
-	gameOverIndex++;
-	iDelayMS(100);
-	if (gameOverIndex == 3){
-		gameOverIndex = 0;
-	}
-	iShowBMP2(370, 260, "images//yourScore.bmp", 0);
-	//convertInt(p, universalScoreVar);
-	show(universalScoreVar, 465, 200);
-	//iText(400, 210,p, GLUT_BITMAP_TIMES_ROMAN_24);
-	iText(300, 70, "PRESS ANY KEY TO RETURN HOME", GLUT_BITMAP_TIMES_ROMAN_24);
-	gameOver = false;
-}
+
 void sun(){
 	iSetColor(247, 127, 0);
 	iFilledCircle(161, 527, 50, 100);
@@ -688,192 +543,15 @@ void virusFactory(){
 }
 
 
+
+
 /************************************************************************NEW GAME****************************************/
-void newGame(){
 
-	int life = 3;
-
-
-	if (musicOn == true && optionMusicOn == true && gameOver == false)
-	{
-		PlaySound("SOUNDS\\runSound.WAV", NULL, SND_LOOP | SND_ASYNC);
-
-		musicOn = false;
-	}
+#include "Navigation.h";
 
 
-	iShowBMP(0, 524, "images//skyBlue.bmp");
-	showCloud();
-	sun();
-	iShowBMP2(350, 670, "images//pill_scr.bmp", 0);
-	iShowBMP2(0, 0, roads[roadIndex], -1);
-
-	iText(10, windowHeight - 30, userName, GLUT_BITMAP_TIMES_ROMAN_24);
-
-	//iDelayMS(10);
-
-	if (!activeViruses.empty()){
-		for (list<Virus>::iterator virus = activeViruses.begin(); virus != activeViruses.end(); virus++){
-			virus->spawn();
-
-			if ((virus->track.getX() + 110 > charecterX&&virus->track.getX() - 110 < charecterX) && virus->track.getY() < charecterY + 100 && virus->hide == false)
-			{
-				point = point - 50;
-				isCollision = true;
-				virus->hide = true;
-				collisionX = virus->track.getX() - 40;
-				collisionY = virus->track.getY();
-			}
-			showExplosion();
-		}
-	}
-
-
-
-
-	if (!jump)
-	{
-		iShowBMP2(charecterX, charecterY, person_run[runningIndex], 0);
-	}
-
-
-	if (jump)
-	{
-
-		iShowBMP2(charecterX, charecterY + jumpY, "images//b14.bmp", 0);
-		jumpY += 20;
-		iDelayMS(10);
-		if (jumpY > 200)
-		{
-			jump = false;
-			jumpY = 0;
-		}
-
-
-
-	}
-
-	//iLine(240, 140, 442, 514);
-	point++;// SHOULD BE CHANGED
-	show(point, 400, 670);// Showing int on screen
-
-	//vy = ((187 * vx) / 101) - (30740 / 101);
-
-	//cout << lt.getY() << endl;
-	/*iShowBMP2(lt.getX(), lt.getY(), "images//virus.bmp", 255);
-	lt.speed(1);*/
-
-
-	/*collision();
-	if(isCollision)
-	showExplosion();*/
-
-	iShowBMP2(windowWidth - 50, windowHeight - 50, "images//heart_filled.bmp", 0);
-	iShowBMP2(windowWidth - 110, windowHeight - 50, "images//heart.bmp", 0);
-	if (gameOver){
-		takeScore = true;
-		gameOverSound = true;
-	}
-	if (takeScore){
-		universalScoreVar = point;
-		setHigh(userName, point);//now for testing this function is taking score after pressing 'l',, it will take score when game over
-		takeScore = false;
-	}
-	if (gameOver == true && takeScore == false){
-		currentPage = "gameOverPage";
-	}
-	int id1 = iLoadImage("images//run1-01.png");
-	iShowImage(50, 50, 150, 250, id1);
-
-}
-void userPage(){
-
-
-	iShowBMP2(200, 500, "images//12.bmp", 0);
-
-	iText(180, 360, ">>", GLUT_BITMAP_TIMES_ROMAN_24);
-	iShowBMP2(200, 100, "images//13.bmp", 0);
-	iSetColor(255, 0, 0);
-	iText(250, 360, userName, GLUT_BITMAP_TIMES_ROMAN_24);
-
-}
-void highScores(){
-	iShowBMP2(350, 600, "images//high.bmp", 0);
-
-	showHigh();// showing high score of all time
-	iShowBMP2(10, 10, "images//home.bmp", 0);
-}
-void settings(){
-
-	iText(260, 575, "Music", GLUT_BITMAP_TIMES_ROMAN_24);
-	iText(260, 475, "Difficulity", GLUT_BITMAP_TIMES_ROMAN_24);
-
-	if (scrollSettingsY == 0 && optionMusicOn == true){
-		iText(470, 575, "ON", GLUT_BITMAP_TIMES_ROMAN_24);
-		musicStateIndex = 0;
-	}
-	else if (scrollSettingsY == 0 && optionMusicOff == true){
-		iText(470, 575, "OFF", GLUT_BITMAP_TIMES_ROMAN_24);
-		musicStateIndex = 1;
-	}
-	else if (scrollSettingsY == 100 && optionDifficulityHigh == true){
-		iText(470, 475, "HIGH", GLUT_BITMAP_TIMES_ROMAN_24);
-		difficulityStateIndex = 2;
-	}
-	else if (scrollSettingsY == 100 && optionDifficulityLow == true){
-		iText(470, 475, "LOW", GLUT_BITMAP_TIMES_ROMAN_24);
-		difficulityStateIndex = 0;
-	}
-	else if (scrollSettingsY == 100 && optionDifficulityMedium == true){
-		iText(470, 475, "MEDIUM", GLUT_BITMAP_TIMES_ROMAN_24);
-		difficulityStateIndex = 1;
-	}
-
-	iRectangle(470, 565 - scrollSettingsY, 200, 50);
-	iText(470, 575, musicState[musicStateIndex], GLUT_BITMAP_TIMES_ROMAN_24);
-	iText(470, 475, difficulityState[difficulityStateIndex], GLUT_BITMAP_TIMES_ROMAN_24);
-	iText(300, 275, "PRESS ARROW KEYS TO NAVIGATE", GLUT_BITMAP_TIMES_ROMAN_24);
-	//iShowBMP2(370, 600, "images//22.bmp", 0);
-	iShowBMP2(10, 10, "images//home.bmp", 0);
-}
-void helpPage(){
-	/*
-	write insructions, rules here.
-	*/
-	iText(250, 200 + scrollY, "write insructions, rules here.", GLUT_BITMAP_TIMES_ROMAN_24);
-	iShowBMP2(10, 10, "images//home.bmp", 0);
-}
-
-
-void creditPage(){
-	iShowBMP(0, 0, "images//creditBack.bmp");
-	iShowBMP2(200, 650, "images//credits.bmp", 0);
-	iLine(200, 625, 500, 625);
-	iShowBMP2(500, 550, "images//naim1.bmp", 0);
-	iShowBMP2(500, 400, "images//omi1.bmp", 0);
-	iShowBMP2(500, 250, "images//rafid1.bmp", 0);
-	iShowBMP2(500, 100, "images//nirjon1.bmp", 0);
-
-	iShowBMP(300, 480, "images//Naim.bmp");
-	iShowBMP(300, 330, "images//omi.bmp");
-	iShowBMP(300, 180, "images//Fahim.bmp");
-	iShowBMP(300, 30, "images//Nirjon.bmp");
-
-
-	iText(500, 525, "ID- 190204081", GLUT_BITMAP_TIMES_ROMAN_24);
-	iText(500, 375, "ID- 190204074", GLUT_BITMAP_TIMES_ROMAN_24);
-	iText(500, 225, "ID- 190204082", GLUT_BITMAP_TIMES_ROMAN_24);
-	iText(500, 75, "ID- 190204063", GLUT_BITMAP_TIMES_ROMAN_24);
-	iDelayMS(10);
-
-	iShowBMP2(10, 10, "images//home_black.bmp", 0);
-
-
-
-	//ImgButton home(10, windowHeight - 60, "images//home.bmp", "homePage");
-	//home.display();
-}
 //bool user = true;
+
 void iDraw()
 {
 	iClear();
@@ -894,7 +572,7 @@ void iDraw()
 			currentPage = "userPage";
 		}
 		else{
-			if (newG || 1){
+			if (newG){
 				newGame();
 			}
 		}
